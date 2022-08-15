e27bd4a1e0023f7268409a2a1c294ecea3a99161
nutsa.matchavariani <nmatchavariani@cu.edu.ge>
2022-08-15 16:05:58 +0400

    emp

---
+++ src/main/java/tech/getarrays/employeemanager/repositories/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package tech.getarrays.employeemanager.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import tech.getarrays.employeemanager.model.Employee;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    void deleteEmployeeById(Long id);
+
+    Optional<Employee> findEmployeeById(Long id);
+}
